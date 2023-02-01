78649fe05091e8321d5016eced87a20e6e03a22f
flefebure <franck.lefebure@softbridge.fr>
2023-02-01 11:23:05 -0400

    Add detach/attach buttons

--- src/main/java/com/test/TestGridRoute.java
+++ src/main/java/com/test/TestGridRoute.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.grid.contextmenu.GridContextMenu;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
@@ -80,8 +81,8 @@ public class TestGridRoute extends VerticalLayout {
         }
         ListDataProvider dataProvider = new ListDataProvider(data);
         Grid grid = new Grid(Item.class);
-        grid.setHeight("400px");
-        grid.setWidth("700px");
+        grid.setHeight("600px");
+        grid.setWidth("1000px");
         grid.setDataProvider(dataProvider);
         grid.setColumnReorderingAllowed(true);
         grid.setMultiSort(true);
@@ -98,15 +99,37 @@ public class TestGridRoute extends VerticalLayout {
 
 
         GridContextMenu gridContextMenu = new GridContextMenu(grid);
-        MenuItemBase item =gridContextMenu.addItem("context menu opened", event -> Notification.show("clicked"));
+        MenuItemBase menuItem =gridContextMenu.addItem("context menu opened", event -> Notification.show("clicked"));
         gridContextMenu.addGridContextMenuOpenedListener(e -> {
             GridContextMenu.GridContextMenuOpenedEvent event = (GridContextMenu.GridContextMenuOpenedEvent)e;
             if (!event.isOpened()) return;;
-            item.setText("context menu opened from column : "+event.getColumnId());
+            menuItem.setText("context menu opened from column : "+event.getColumnId());
         });
 
-        setHeight("400px");
+        setHeightFull();
         add(grid);
+
+        grid.setItemDetailsRenderer(
+                new ComponentRenderer(activity -> {
+                    VerticalLayout layout = new VerticalLayout();
+                    layout.setSizeFull();
+                    layout.setPadding(false);
+                    layout.setSpacing(false);
+                    for (int i = 0; i < 5; i++) {
+                        Item item = new Item("AAA"+i, "BBB"+i, "CCC"+i, "DDD"+i, "EEE"+i);
+                        data.add(item);
+                    }
+                    ListDataProvider dataProvider2 = new ListDataProvider(data);
+                    Grid grid2 = new Grid(Item.class);
+                    grid2.setWidthFull();
+                    grid2.setDataProvider(dataProvider2);
+                    grid2.getColumnByKey("column1").setWidth("200px").setResizable(true);
+                    grid2.getColumnByKey("column2").setWidth("200px").setResizable(true);
+                    grid2.getColumnByKey("column3").setWidth("200px").setResizable(true);
+                    layout.add(grid2);
+                    return layout;
+                }));
+
 
         Button freeze = new Button("freeze 2 first columns", event -> {
             grid.getColumnByKey("column1").setFrozen(true);
@@ -116,8 +139,32 @@ public class TestGridRoute extends VerticalLayout {
             grid.getColumnByKey("column1").setFrozen(false);
             grid.getColumnByKey("column2").setFrozen(false);
         });
+        Button showPanel = new Button("show panel detail with embedded grid", event -> {
+            Item item  = data.get(5);
+            grid.setDetailsVisible(item, true);
+        });
+        Button hidePanel = new Button("close panel detail with embedded grid", event -> {
+            Item item  = data.get(5);
+            grid.setDetailsVisible(item, false);
+        });
+        Button detachGrid = new Button("detach grid", event -> {
+            remove(grid);
+        });
+        Button attachGrid = new Button("attach grid", event -> {
+            addComponentAtIndex(0, grid);
+        });
+        Button fullResync = new Button("full resync", event -> {
+           Notification.show("resync", 5000, Notification.Position.BOTTOM_END);
+        });
+
         add(freeze);
         add(unfreeze);
+        add(showPanel);
+        add(hidePanel);
+        add(detachGrid);
+        add(attachGrid);
+        add(fullResync);
+
     }
 
 }
